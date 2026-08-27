b94baf655164adb754ee7ac252f94425e16951ab
hobbes6-2 <shreya.deb@gmail.com>
2026-08-27 16:43:22 +0530

    works, must do UI

--- src/app.jsx
+++ src/app.jsx
@@ -7,13 +7,25 @@ export default function App() {
     if (!file) return;
 
     const lib = await extractLib(file);
-    const scripts = lib.forEach(xml => extractAS(xml))
+    const scripts = await Promise.all(lib.map(entry => extractAS(entry, entry.filename)));
     console.log(lib, scripts);
+    let ul = document.getElementById('scriptList');
+    scripts.forEach(file => {
+      let li = document.createElement('li');
+      let btn = document.createElement('button');
+      btn.textContent = Object.keys(file)[0];
+      btn.onclick = openAce;
+      li.append(btn);
+      ul.append(li);
+    })
   }
+
+  function openAce(){}
 
   return (
     <div id="app">
       <input type='file' accept='.fla' id="fileIn" onChange={handleFile} />
+      <ul id="scriptList"></ul>
     </div>
   )
 }
--- src/extractAS.js
+++ src/extractAS.js
@@ -1,13 +1,25 @@
+import {TextWriter} from '@zip.js/zip.js'
+
 /**
  * Extract the ActionScript from an XML file.
- * @param {File} file - The XML
+ * @param {ZipFileEntry} file - The XML
+ * @param {string} filename - The filename
  * @param {boolean} binary - Is this a binary FLA?
  * @returns {object} - Object holding every script in the FLA.
  */
-export default function extractAS(file, binary = false){
+export default async function extractAS(file, filename = 'unnamed_file', binary = false){
     if(binary) console.log('Binary FLAs unsupported right now');
     let scripts = {};
+    let text = await file.getData(new TextWriter());
     let parser = new DOMParser();
-    parser.parseFromString(file);
+    let doc = parser.parseFromString(text, 'application/xml');
+    let nm = filename;
+    nm.replace('.xml', '')
+    if (filename == 'unnamed_file') {
+        const timeline = doc.getElementsByTagName('DOMTimeline')[0];
+        if (timeline) nm = timeline.name;
+    }
+    const nodes = [...doc.getElementsByTagName('script')].map(node => node.textContent);
+    if (nodes.length) scripts[nm] = nodes;
     return scripts;
 }
